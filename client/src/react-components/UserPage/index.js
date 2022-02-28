f997bfcbb046921aca898216c5483cad068a28c4
Kuai Yu <quick2063706271@outlook.com>
2022-02-27 23:26:56 -0500

    update userpage

--- client/src/react-components/UserPage/index.js
+++ client/src/react-components/UserPage/index.js
@@ -2,14 +2,25 @@ import React from "react";
 import "./styles.css";
 import PersonalInformation from "../PersonalInformation";
 import NavigationMenu from "../NavigationMenu";
+import HistoryItem from "../HistoryItem";
 import SideMenu from "../SideMenu";
+import AppBar from "../AppBar";
 
 class UserPage extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            selectedPanel: 0
+        }
+    }
+
     render() {
         return (
             <div>
+                <AppBar/>
                 <SideMenu/>
                 <PersonalInformation/>
+                {/* <HistoryItem/> */}
             </div>
         );
     }
