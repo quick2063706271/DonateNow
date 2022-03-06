ce66fc5a31771a6d7580b4a3679f3d95a3309221
Yinuo Qin <yinuoqin@Yinuos-MacBook-Pro.local>
2022-03-05 19:08:13 -0500

    add feedback page

--- client/src/react-components/AdminFeedback/index.js
+++ client/src/react-components/AdminFeedback/index.js
@@ -1,10 +1,26 @@
 import React from "react";
 import "./styles.css";
+import database from '../../database'
+import AdminAppBar from "../AdminAppBar";
+import { uid } from "react-uid";
+import StickyFooter from "../StickyFooter";
 
 class AdminFeedback extends React.Component {
     render() {
         return (
-            <div></div>
+            <div>
+                <AdminAppBar/>
+                <div className="blocklist">
+                    <div className="header">
+                        <h1><b>All User Feedbacks:</b></h1>
+                    </div>
+                </div>
+
+              <div>
+                  <StickyFooter/>
+              </div>
+
+            </div>
         );
     }
 
