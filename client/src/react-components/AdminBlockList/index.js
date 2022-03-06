33c8fc5ea28f0a920d6316cf328d49d916c13b92
Yinuo Qin <yinuoqin@Yinuos-MacBook-Pro.local>
2022-03-05 23:20:17 -0500

    update block user page

--- client/src/react-components/AdminBlockList/index.js
+++ client/src/react-components/AdminBlockList/index.js
@@ -3,6 +3,7 @@ import "./styles.css";
 import database from '../../database'
 import AdminAppBar from "../AdminAppBar";
 import { uid } from "react-uid";
+import { Button } from '@mui/material';
 import Table from "@material-ui/core/Table";
 import TableBody from "@material-ui/core/TableBody";
 import TableCell from "@material-ui/core/TableCell";
@@ -11,6 +12,7 @@ import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
 import Paper from "@material-ui/core/Paper";
 import StickyFooter from "../StickyFooter";
+
 
 
 function createData(number, username, numCom, action) {
@@ -27,7 +29,30 @@ const rows = [
 
 class AdminBlockList extends React.Component {
 
+    constructor() {
+        super();
+        this.state = {
+            isBlocked: true
+        }
+        this.handleBlock.bind(this);
+        console.log(this.state.isBlocked)
+    }
+
+    handleBlock = event => {
+        this.setState({
+            accountBlocked: !this.state.accountBlocked
+        });
+        this.props.accountBlocked = true;
+        // this.changeButtonText();
+        console.log(this.state.accountBlocked);
+    }
+
     render() {
+        const users = database.users;
+
+        let action;
+        action = <Button variant="contained" display="inline-block" onClick={this.handleBlock}> {this.state.accountBlocked? "Block the User" : "Blocked" } </Button>
+
         return (
 
             <div>
@@ -56,7 +81,9 @@ class AdminBlockList extends React.Component {
                         </TableCell>
                         <TableCell align="center">{row.username}</TableCell>
                         <TableCell align="center">{row.numCom}</TableCell>
-                        <TableCell align="center">{row.action}</TableCell>
+                        <TableCell align="center">{action}</TableCell>
+                        {/*<TableCell align="center">{row.action}</TableCell>*/}
+
                       </TableRow>
                     ))}
                   </TableBody>
