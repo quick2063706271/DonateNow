1078bf294ba135b8ff93de73d21631fe35826f79
Sailing Ni <sailing.inbox@gmail.com>
2022-03-27 23:02:34 -0400

    edit mongoose uri

--- db/mongoose.js
+++ db/mongoose.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose')
 
 /* Connnect to our database */
 // Get the URI of the local database, or the one specified on deployment.
-const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/StudentAPI'
+const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/DonateNowAPI'
 
 mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true});
 
