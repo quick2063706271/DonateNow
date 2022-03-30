ba08e73f60b0cc49f1effa2f28d149c242b273fe
Kuai Yu <quick2063706271@outlook.com>
2022-03-30 01:48:10 -0400

    update post.js

--- server/models/post.js
+++ server/models/post.js
@@ -1,4 +1,79 @@
-/* User model */
+/* Post model */
 'use strict';
 
 const mongoose = require('mongoose')
+const validator = require('validator')
+
+const PostSchema = new mongoose.Schema({
+    ownerId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "ownerId"
+    },
+    ownerStatus: {
+        type: String,
+        minlength: 1,
+        required: true,
+        trim: true
+    },
+    viewers: [ViewerStatusSchema],
+    imageSrc: {
+        type: String // change later
+    },
+    deliveryOption: {
+        type: String,
+        minlength: 1,
+        required: true,
+        trim: true
+    },
+    header: {
+        type: String,
+        minlength: 1,
+        required: true,
+        trim: true
+    },
+    location: {
+        type: String,
+        minlength: 1,
+        required: true,
+        trim: true
+    },
+    description: {
+        type: String,
+        minlength: 1,
+        required: true,
+        trim: true
+    },
+    categories: [{
+        type: String,
+        minlength: 1,
+        trim: true
+    }],
+    blocked: {
+        type: Number
+    },
+    views: { 
+        type: Number
+    },
+    saved: { // What is saved?
+        type: Number
+    },
+    datePosted: {
+        type: String,
+        minlength: 1,
+        required: true,
+        trim: true
+    },
+})
+
+const ViewerStatusSchema = new mongoose.Schema({
+    viewerId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "ownerId"
+    },
+    viewStatus: {
+        type: String,
+        minlength: 1,
+        required: true,
+        trim: true
+    }
+})
