4856f23c7c5907a3e84978d4a6172981ab4ef6fe
JiahuaChen254 <amandaaa.chen@mail.utoronto.ca>
2022-03-31 00:28:06 -0400

    login page partial fix

--- client/src/actions/user.js
+++ client/src/actions/user.js
@@ -28,21 +28,79 @@ export const checkSession = (app) => {
     
 };
 
-export const getUser = (app) => {
-    const url = "${APP_HOST}/userpage"
+// A functon to update the login form state
+export const updateLoginForm = (loginComp, field) => {
+    const value = field.value;
+    const name = field.name;
 
-    fetch(url)
+    loginComp.setState({
+        [name]: value
+    });
+};
+
+// A function to send a POST request with the user to be logged in
+export const login = (loginComp, app) => {
+    // Create our request constructor with all the parameters we need
+    const request = new Request(`${API_HOST}/login`, {
+        method: "post",
+        body: JSON.stringify(loginComp.state),
+        headers: {
+            Accept: "application/json, text/plain, */*",
+            "Content-Type": "application/json"
+        }
+    });
+    // Send the request with fetch()
+    fetch(request)
         .then(res => {
             if (res.status === 200) {
-                return res.json;
-            } else {
-                alert("Could not get user")
+                return res.json();
             }
         })
         .then(json => {
-            SideMenu.setState({}) // to-do
+            if (json.userId !== undefined) {
+                //app.setUserId({ userId: json.userId });
+                app.setUserId(json.userId)
+                console.log(loginComp)
+                loginComp.setLoginState(json.userId, false, true)
+            }
         })
         .catch(error => {
-            console.log(error)
-        })
+            console.log(error);
+            loginComp.setLoginState(-1, true, false)
+        });
 };
+
+// A function to send a GET request to logout the current user
+export const logout = (app) => {
+    const url = `${API_HOST}/logout`;
+
+    fetch(url)
+        .then(res => {
+            app.setState({
+                userId: null,
+                message: { type: "", body: "" }
+            });
+        })
+        .catch(error => {
+            console.log(error);
+        });
+};
+
+// export const getUser = (app) => {
+//     const url = "${APP_HOST}/userpage"
+
+//     fetch(url)
+//         .then(res => {
+//             if (res.status === 200) {
+//                 return res.json;
+//             } else {
+//                 alert("Could not get user")
+//             }
+//         })
+//         .then(json => {
+//             SideMenu.setState({}) // to-do
+//         })
+//         .catch(error => {
+//             console.log(error)
+//         })
+// };
