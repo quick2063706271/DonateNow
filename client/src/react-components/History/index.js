7c2f87230a90a7987aa4cc097a1df04ecf3003b8
Kuai Yu <quick2063706271@outlook.com>
2022-04-01 15:31:32 -0400

    Donated History can now works

--- client/src/react-components/History/index.js
+++ client/src/react-components/History/index.js
@@ -2,17 +2,51 @@ import "./styles.css"
 import React from "react";
 import { uid } from "react-uid";
 import HistoryItem from "../HistoryItem";
+import { getDonationHistory } from "../../actions/user";
+import { checkSession } from "../../actions/user";
 
 class History extends React.Component {
     constructor() {
         super()
+        this.state = {
+            donationPosts: [],
+            transactedPosts: []
+        }
+    }
+
+    fetchHistoryInformation = () => {
+        getDonationHistory(this)
+        // findPostByKeyword(this, keyword)
+    }
+    componentDidMount() {
+        checkSession(this, this.fetchHistoryInformation()); // sees if a user is logged in
     }
     render() {
+        console.log(this.state)
         const {items, category} = this.props;
+        console.log(category)
         let histories
-        if (items.length === 0) {
+        if (this.state.donationPosts.length === 0) {
             histories = <div id="history-container"></div>
-        } else {
+        } 
+        if (category === "donation") {
+            console.log("goes to condition 2")
+            console.log(this.state.donationPosts)
+            histories = <div id="history-container">
+                            {this.state.donationPosts.map((item) => (<HistoryItem 
+                                                    key={uid(item)} 
+                                                    header={item.header} 
+                                                    status={item.ownerStatus} 
+                                                    date={item.datePosted}
+                                                    img={item.imageSrc}
+                                                    postId={item.postId}
+                                                />)
+                            )}
+                        </div>
+        }
+        
+        
+        else {
             histories = <div id="history-container">
                             {items.map((item) => (<HistoryItem 
                                                     key={uid(item)} 
