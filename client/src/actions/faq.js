70f32899b0295fe25fb2af76a82f822109862f43
Peppermint-Chocolate <yinuoqin01194@gmail.com>
2022-04-01 15:14:05 -0400

    update faq

---
+++ client/src/actions/faq.js
@@ -0,0 +1,32 @@
+// environment configutations
+import ENV from './../config.js'
+const API_HOST = ENV.api_host
+// console.log('Current environment:', ENV.env)
+
+export const getFaqs = (app) => {
+    console.log(app.state)
+    const request = new Request(`${API_HOST}/api/faqpage`, {
+        method: "GET",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+    });
+    // Send the request with fetch()
+    fetch(request)
+        .then(res => {
+            if (res.status === 200) {
+                return res.json();
+            }
+        })
+        .then(json => {
+            if (json) {
+                console.log(json)
+                app.setState({
+                    terms: json
+                }, () => { console.log(app.state) });
+            }
+        })
+        .catch(error => {
+            console.log(error);
+        });
+}
